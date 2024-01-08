e09160f023b12e980423e724cef355f033629c03
Pal <sachin.pal@hpe.com>
2024-01-08 21:48:50 +0530

    added allowed host

--- NGO/NGO/settings.py
+++ NGO/NGO/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DEBUG')
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS += os.environ.get('ALLOWED_HOST').split()
 
 # Application definition
 
